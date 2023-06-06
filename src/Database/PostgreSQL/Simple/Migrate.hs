5d88ea6a172eb2fe5e3626531fec8f70bcb9cc70
Brian Hurt <bhurt42@gmail.com>
2023-06-06 10:29:10 -0500

    More exports.

--- src/Database/PostgreSQL/Simple/Migrate.hs
+++ src/Database/PostgreSQL/Simple/Migrate.hs
@@ -2,9 +2,14 @@
 module Database.PostgreSQL.Simple.Migrate (
     -- * Migration type
     Migration(..),
+    Optional(..),
     makeMigration,
-    Replaces,
-    makeReplaces
+    Replaces(..),
+    makeReplaces,
+    apply,
+    makeFingerprint
 ) where
 
+    import Database.PostgreSQL.Simple.Migrate.Internal.Apply
+    import Database.PostgreSQL.Simple.Migrate.Internal.Finger
     import Database.PostgreSQL.Simple.Migrate.Internal.Types
