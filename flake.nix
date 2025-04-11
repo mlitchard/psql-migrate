92d241ba36b3695e85a9759a9076f1277399886a
Michael Litchard <michael.litchard@platonic.systems>
2025-04-11 17:32:35 +0000

    some type of flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "horizon-platform-template";
+  description = "psql-migrate";
 
   nixConfig = {
     extra-substituters = "https://horizon.cachix.org";
@@ -7,39 +7,110 @@
   };
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    horizon-devtools.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-platform?ref=lts/ghc-9.8.x";
+    flake-utils.url = "github:numtide/flake-utils";
+    horizon-devtools.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-devtools?ref=lts/ghc-9.8.x";
     horizon-platform.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-platform?ref=lts/ghc-9.8.x";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs =
-    inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
-      perSystem = { pkgs, system, ... }:
-        let
-          myOverlay = final: prev: {
-            psql-migrate = final.callCabal2nix "psql-migrate" ./. { };
-          };
-          legacyPackages = inputs.horizon-platform.legacyPackages.${system}.extend myOverlay;
-        in
-        rec {
+    inputs@
+    { self
+    , flake-utils
+    , horizon-platform
+    , horizon-devtools
+    , nixpkgs
+    , ...
+    }: with builtins; let
+      inherit (inputs.nixpkgs) lib;
+      onlyHaskell = fs2source
+        (fs.union
+          (onlyExts-fs [ "cabal" "hs" "project" ] ./.)
+          ./LICENSE # horizon requires this file to build
+        );
+      fs = lib.fileset;
+      fs2source = fs': path: fs.toSource { root = path; fileset = fs'; };
+      onlyExts-fs = exts: fs.fileFilter (f: foldl' lib.or false (map f.hasExt exts));
+      onlyExts = exts: path: fs2source (onlyExts-fs exts path) path;
 
-          devShells.default = legacyPackages.shellFor {
-            packages = p: [ p.psql-migrate ];
-            buildInputs = [
-              legacyPackages.cabal-install
-            ];
-          };
+    in
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ]
+      (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        hlib = pkgs.haskell.lib;
+        devtools = inputs.horizon-devtools.packages.${system};
 
-          inherit legacyPackages;
-
-          packages = rec {
-            inherit (legacyPackages)
-              psql-migrate;
-            default = packages.psql-migrate;
-          };
-
+        legacyPackages =
+          horizon-platform.legacyPackages.${system}.extend
+            self.haskell-overlay.${system}.default;
+      in
+      {
+        haskell-overlay.default = final: prev: {
+          psql-migrate = final.callCabal2nix "psql-migrate" (onlyHaskell ./.) { };
         };
-    };
+
+        packages.default = legacyPackages.psql-migrate;
+
+        devShells.default = legacyPackages.psql-migrate.env.overrideAttrs (attrs: {
+          buildInputs = attrs.buildInputs ++ (with pkgs; [
+            cabal-install
+            haskellPackages.cabal-fmt
+          ] ++ lib.optionals (system == "x86_64-linux" || system == "aarch64-linux") [
+            devtools.ghcid
+            devtools.hlint
+            devtools.haskell-language-server
+          ] ++ [
+            nixpkgs-fmt
+            stylish-haskell
+          ]);
+
+          shellHook =
+            let
+              instructions = ''
+                ${write-descriptions general-functions}
+              '';
+
+              general-functions = {
+                format = {
+                  description = "format code";
+                  body = ''
+                    nix fmt
+                    stylish-haskell -ir .
+                    cabal-fmt -i *.cabal
+                  '';
+                };
+
+                shelp = {
+                  description = "show this message";
+                  body = "echo ${lib.escapeShellArg instructions}";
+                };
+              };
+
+
+              write-set = set: f: concatStringsSep "\n" (lib.mapAttrsToList f set);
+
+              write-descriptions = set: ''
+                ${write-set set (n: v: "  ${n}\t${v.description}")}'';
+            in
+            ''
+              ${write-set (general-functions)
+                  (n: v: ''
+                     ${n}() {
+                       ${v.body}
+                     }
+                   '')
+              }
+
+              shelp
+            '';
+        });
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
 }
